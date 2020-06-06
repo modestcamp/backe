8f34671146065b7f973bf1f82a1ba59e8fe741ef
modestcamp <ggcampoy@gmail.com>
2020-06-05 21:12:47 -0500

    adelanto

--- Api/index.js
+++ Api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Users} = require('../models');
 
 const api = express();
 
@@ -7,6 +8,19 @@ api.use(express.json({ extended: true}));
 
 api.get('/', (req, res) => {
     res.json({ message: 'Hello m8'})
+});
+
+api.use('/api');
+
+// Users 
+
+
+api.post('/users', (req, res) => {
+    const newUser = new User(req.body);
+    
+    newUser.save()
+    .then((user) => res.status(201).json(user))
+    .catch((err) => res.status(400).json(err));
 });
 
 module.exports = api;
